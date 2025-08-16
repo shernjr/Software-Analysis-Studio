283e8c093a72e68971b1c4236823c0c2becf8d8e
shernjr <38554378+shernjr@users.noreply.github.com>
2025-08-16 18:55:58 +1000

    updating
    new distro (fedora), redoing all.

---
+++ Assignment-1/CMakeFiles/ass1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass1.dir/link.d"
+  "CMakeFiles/ass1.dir/Assignment-1.cpp.o"
+  "CMakeFiles/ass1.dir/Assignment-1.cpp.o.d"
+  "CMakeFiles/ass1.dir/test-exes/Test.cpp.o"
+  "CMakeFiles/ass1.dir/test-exes/Test.cpp.o.d"
+  "bin/ass1"
+  "bin/ass1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
